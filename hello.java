c8e3f493f4f9b006c5f15601466650ccf5a4bc60
JOSÉ <38122623808@educacional.up>
2022-03-09 21:43:13 -0300

    class increments

--- hello.java
+++ hello.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 class hello {
     public static void main(String[] args) { 
         int valor = 10;
         String text = "o valor de tal coisa é";
-        System.out.println(text+" "+valor);
+        System.out.println(text+" "+valor+"\n");
+        String welcomeMsg = "olá";
+        String welcomeMsgLast = "Para você";
+        System.out.println(welcomeMsg+" "+welcomeMsgLast);
+
     }
 }
